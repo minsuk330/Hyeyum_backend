f88d1b80a3e100ad74a9f0769b963eb61c3da6dc
watoo <minsuk33011@gmail.com>
2025-10-30 23:53:51 +0900

    feat: first commit

---
+++ src/test/java/com/konkuk/gongmojeon/GongmojeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.gongmojeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongmojeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
